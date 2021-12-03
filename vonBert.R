40ebccc4bad00ea6bacccbd4a644f3c0497807dc
Tal Ben-Horin PhD <tbenhor@ncsu.edu>
2021-12-03 15:33:58 -0500

    update

--- vonBert.R
+++ vonBert.R
@@ -21,16 +21,38 @@ library(scales)
 library(ggdistribute)
 
 dat <- read.csv("dlvb.csv", fill = FALSE, header = TRUE)
-m1 <-list(dl=dat$dl,line=dat$line,t=dat$days) #data string
+m1 <-list(dl=dat$dl,line=dat$line,dt=dat$days,ind=dat$ind,L=dat$length) #data string
 
 # individual and group von Bertalanffy growth model 
 cat(
   "model{
     for (i in 1:XX) {
-
-  }",
+    #
+    dl[i] ~ dnorm(mu[i],tau)
+    mu[i] <- (Linf - L[i])*(1-exp(-K[ind[i]]*dt))
+    }
+    for (l in 1:XX) {
+      K[l] ~ dnorm(mu_line[l],tau_line[l])
+    }
+    mu_line ~ dnorm(0,0.1)
+    tau ~ dgamma(0.1,0.1)
+    tau_line ~ dgamma(0.1,0.1)
+  ",
   file="m1.jag"
 )
+
+cat(
+  "model{
+    for (i in 1:5320) {
+      # Observation model across serial dilutions
+      c[i] ~ dbin(p[i],3)
+      p[i] <- 1-exp(-MPN[i]*v[i])
+      
+      # Biological model for microbial abundance
+      MPN[i] ~ dpois(lambda[i])
+      log(lambda[i]) <- b0 + b1*gear[i] + b2*tide[i] + b3*gear[i]*tide[i] + b4*mod[i] + b5*hi[i] + U[samp[i]] +V[time[i]]
+    }
+
 
 mfit <- jags(data = m1,
                inits = m1.inits,
